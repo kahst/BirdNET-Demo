2ce437fbc7e85274f44105c4da7879796c40acbf
Stefan Kahl <ykahst@hrz.tu-chemnitz.de>
2019-12-20 14:41:11 +0100

    Run on localhost

--- static/js/stream.js
+++ static/js/stream.js
@@ -168,21 +168,21 @@ function requestStreamAnalysis() {
 $( document ).ready(function() {
 
     // For now, we need to click the canvas in order to start the visualization
-    $('#spec').click(function() {
+    //$('#spec').click(function() {
 
-        // Adjust canvas size
-        $("#spec").width($("#spec-holder").width());
-        $("#spec").height($( window ).height() * 0.4);
+    // Adjust canvas size
+    $("#spec").width($("#spec-holder").width());
+    $("#spec").height($( window ).height() * 0.4);
 
-        // Start audio
-        console.log('Starting playback...');
-        var base_canvas = document.getElementById('spec');
-        var aud = document.getElementById('player');        
-        aud.play();        
-        
-        // Start spectrogram viewer
-        var viewer = new AudioViewer(base_canvas, aud, 1024, 1024, $('#spec').width(), $('#spec').height());
-    });
+    // Start audio
+    console.log('Starting playback...');
+    var base_canvas = document.getElementById('spec');
+    var aud = document.getElementById('player');        
+    aud.play();        
+    
+    // Start spectrogram viewer
+    var viewer = new AudioViewer(base_canvas, aud, 1024, 1024, $('#spec').width(), $('#spec').height());
+    //});
 
     // Request analysis results every second
     requestStreamAnalysis();
