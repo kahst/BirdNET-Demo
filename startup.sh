9b337e5c4b333067966def6bc95e62bb0c011834
Stefan Kahl <ykahst@hrz.tu-chemnitz.de>
2020-03-09 10:07:25 +0100

    Wait 25 seconds

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ echo 'STARTING BirdNET ANALYZER...'
 python3 analyzer.py &> /tmp/birdnet_analyzer.log &
 echo 'STARTING BirdNET SERVER...'
 python server.py &
-echo 'WAITING FOR 5 SECONDS...'
-sleep 5
+echo 'WAITING FOR 25 SECONDS...'
+sleep 25
 echo 'STARTING CHROMIUM BROWSER...'
 chromium-browser --kiosk --autoplay-policy=no-user-gesture-required --incognito --password-store=basic --app=http://localhost:8080
 echo 'DONE! STARTUP COMPLETE!'
