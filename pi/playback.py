72fa2883157821b493ef7db6190d51b10d921ab0
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2020-03-09 14:01:00 +0100

    New sounds

--- pi/playback.py
+++ pi/playback.py
@@ -6,10 +6,10 @@ import threading
 import RPi.GPIO as GPIO
 
 # Define sounds
-SOUNDS = {11: {'audio': ['CS_01.wav', 'CS_02.wav'], 'last_action': 0},
-          12: {'audio': ['CHM_01.wav', 'CHM_02.wav'], 'last_action': 0},
-          13: {'audio': ['CL_01.wav', 'CL_02.wav'], 'last_action': 0},
-          15: {'audio': ['EPF_01.wav', 'EPF_02.wav'], 'last_action': 0},
+SOUNDS = {11: {'audio': ['CS_01.wav', 'CS_02.wav', 'CS_06.wav', 'CS_07.wav', 'CS_08.wav', 'CS_09.wav'], 'last_action': 0},
+          12: {'audio': ['CHM_01.wav', 'CHM_02.wav', 'CHM_03.wav', 'CHM_04.wav', 'CHM_05.wav'], 'last_action': 0},
+          13: {'audio': ['CL_01.wav', 'CL_02.wav', 'CL_03.wav', 'CL_05.wav', 'CL_06.wav'], 'last_action': 0},
+          15: {'audio': ['EPF_01.wav', 'EPF_02.wav', 'EPF_03.wav', 'EPF_04.wav', 'EPF_05.wav'], 'last_action': 0},
           16: {'audio': ['CCC_01.wav', 'EBT_01.wav', 'EGF_01.wav', 'GC_01.wav', 'YH_01.wav'], 'last_action': 0},
           18: {'audio': [], 'last_action': 0},
           22: {'audio': [], 'last_action': 0}
@@ -52,11 +52,12 @@ def playRandomSound():
             return None
 
     # Check for last random playback
-    if not now - LAST_RANDOM_PLAYBACK < 10:
-        button_callback(random.choice([11, 12, 13, 15, 16]))
+    if not now - LAST_RANDOM_PLAYBACK < random.randint(10, 30):
+        LAST_RANDOM_PLAYBACK = now
+        button_callback(random.choice([11, 12, 13, 15, 16]), True)
 
 # Button callback
-def button_callback(channel):
+def button_callback(channel, isRandom=False):
 
     global SOUNDS
 
@@ -67,7 +68,8 @@ def button_callback(channel):
     if now - SOUNDS[channel]['last_action'] > 1.5:
         
         # Save time of last button press
-        SOUNDS[channel]['last_action'] = now
+        if not isRandom:
+            SOUNDS[channel]['last_action'] = now
 
         # Randomly select a file to play
         afile = random.choice(SOUNDS[channel]['audio'])
@@ -93,6 +95,8 @@ while True:
     except KeyboardInterrupt:
         print('TERMINATED')
         break
+    except:
+        continue
 
 # Clean up
 print('CLEANING UP')
